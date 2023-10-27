45f6608576ebf31d8122fb2e92e016bff5dc5920
Congalala <149178820+Congalala@users.noreply.github.com>
2023-10-27 09:27:38 -0500

    Add files via upload

---
+++ pokedexNeira/js/main.js
@@ -0,0 +1,173 @@
+let orquideas = [
+    {
+        id: 1,
+        nombre: "Lepanthes escifera",
+        imagen: "img/objEscifera.png",
+        habito: "Hierba, Epífita",
+        size: '0,12" [3 mm]',
+        familia: "Orchidaceae",
+        origen: "Endémica",
+        region: "Andes",
+        elevacion: "1400 - 2150 msnm",
+        departamentos: "Antioquia, Risaralda, Santander",
+        conservacion: "No Evaluada",
+        descripcion: "Epífita de tamaño miniatura, crecimiento fresco a frío con ramicauls suberectos a transversales, muy delgados envueltos por 6 a 11 vainas microscópicamente escabrosas, lepantiformes y portando una única hoja de base peciolada, extendida a colgante, coriácea, púrpura por debajo, ovada, aguda, largamente acuminada, redondeada y abruptamente contraída por debajo, que florece sobre una hoja filiforme, muy congestionada, dísticas, incluido el pedúnculo, inflorescencia de una sola flor sucesiva que surge en la parte superior de la hoja.",
+        referencias: `Farfán et al. (2003), Viveros & Higgins (2007), Luer & Thoerle (2012).
+        Viveros, P. , C.A. Luer & M. Celis 2023-10-10. Lepanthes agglutinata Luer En Bernal, R., S.R. Gradstein & M. Celis (eds.). 2015. Catálogo de plantas y líquenes de Colombia. Instituto de Ciencias Naturales, Universidad Nacional de Colombia, Bogotá. http://catalogoplantasdecolombia.unal.edu.co.
+        W3 Tropicos, Kew Monocot list, IPNI, AOS Bulletin Vol 53 No 1, 1984.`
+    },
+    {
+        id: 2,
+        nombre: "Lepanthes alcicornis",
+        imagen: "img/objAlcicornis.jpg",
+        habito: "Hierba, Epífita",
+        size: '08" [2mm]',
+        familia: "Orchidaceae",
+        origen: "Endémica",
+        region: "Andes",
+        elevacion: "1730 - 2320 msnm",
+        departamentos: "Antioquia, Risaralda, Santander",
+        conservacion: "No Evaluada",
+        descripcion: "Epífita de tamaño miniatura de crecimiento frío, con ramicauls delgados y erectos, envueltos por 10 a 12 vainas lepantiformes con ostias ciliadas cortas y dilatadas, y que llevan una sola, apical, erecta, coriácea, más o menos impregnada de rojo, ovada, aguda, acuminada, cuneada y contraída por debajo en la hoja de base peciolada y que florece en un racimo compacto, dístico, filiforme, con muchas flores sucesivas que surgen en la parte superior de la hoja.",
+        referencias: `Farfán et al. (2003), Viveros & Higgins (2007), Luer & Thoerle (2012).
+        Viveros, P. , C.A. Luer & M. Celis 2023-10-10. Lepanthes alcicornis Luer & R.Escobar En Bernal, R., S.R. Gradstein & M. Celis (eds.). 2015. Catálogo de plantas y líquenes de Colombia. Instituto de Ciencias Naturales, Universidad Nacional de Colombia, Bogotá. http://catalogoplantasdecolombia.unal.edu.co
+        W3 Tropicos, Kew Monocot list, IPNI, Amer. Orchid Soc. Bull. 53: 368 Luer & Escobar 1984.`
+    },
+    {
+        id: 3,
+        nombre: "Lepanthes cerambyx",
+        imagen: "img/objCerambyx.jpg",
+        habito: "Hierba, Epífita",
+        size: '0.2" [5 mm]',
+        familia: "Orchidaceae",
+        origen: "Endémica",
+        region: "Andes",
+        elevacion: "1600 - 2000 msnm",
+        departamentos: "Antioquia",
+        conservacion: "No Evaluada",
+        descripcion: "Se reconoce por sus hojas estrechamente ovadas que llevan en la superficie dorsal una pequeña flor colorida con lóbulos superiores de los pétalos prominentes, erectos y oblicuamente truncados, que recuerdan a los cuernos de un escarabajo. Los sépalos son anchos y obtusos, y los lóbulos del labio son más pequeños que la columna.",
+        referencias: `Farfán et al. (2003), Viveros & Higgins (2007), Govaerts et al. (2012), Luer & Thoerle (2012)
+        Viveros, P. , C.A. Luer & M. Celis 2023-10-10. Lepanthes cerambyx Luer & R.Escobar En Bernal, R., S.R. Gradstein & M. Celis (eds.). 2015. Catálogo de plantas y líquenes de Colombia. Instituto de Ciencias Naturales, Universidad Nacional de Colombia, Bogotá. http://catalogoplantasdecolombia.unal.edu.co
+        W3 Tropicos, Kew Monocot list, IPNI, Orquideología 18(1): 32–34 Luer & Escobar 1991.`
+    },
+    {
+        id: 4,
+        nombre: "Lepanthes agglutinata",
+        imagen: "img/objAgglutinata.jpg",
+        habito: "Hierba, Epífita",
+        size: '0,1" [2 mm]',
+        familia: "Orchidaceae",
+        origen: "Nativa en Colombia",
+        region: "Andes",
+        elevacion: "1730 - 2320 msnm",
+        departamentos: "Antioquia, Caquetá, Chocó, Nariño, Santander, Valle",
+        conservacion: "No Evaluada",
+        descripcion: "Epífita de tamaño pequeño con ramicauls delgados y erectos envueltos por 8 a 16 vainas lepantiformes, minuciosamente ciliadas y que llevan una sola vaina apical, erecta, hoja finamente coriácea, a menudo teñida de púrpura, ovada, aguda, larga acuminada, cuneada por debajo en la base redondeada y contraída peciolada que florece en una hoja delgada en el dorso de esta,  inflorescencia racemosa, congestionada, con muchas flores sucesivamente. Se distingue principalmente por el racimo muy congestionado y con muchas flores que se encuentra detrás de la hoja, a menudo teñida de púrpura, que lleva flores bastante grandes y translúcidas con pétalos de color verde esmeralda con bordes de color púrpura oscuro. El apéndice es comparativamente grande y tiene forma de barco.",
+        referencias: `Farfán et al. (2003), Viveros & Higgins (2007), Govaerts et al. (2012), Luer & Thoerle (2012)
+        Viveros, P. , C.A. Luer & M. Celis 2023-10-10. Lepanthes agglutinata Luer En Bernal, R., S.R. Gradstein & M. Celis (eds.). 2015. Catálogo de plantas y líquenes de Colombia. Instituto de Ciencias Naturales, Universidad Nacional de Colombia, Bogotá. http://catalogoplantasdecolombia.unal.edu.co
+        W3 Tropicos, Kew Monocot list , IPNI ; Phytologia 54:327 Luer 1983; AOS Bulletin 53[9]: 922, Luer & Escobar 1984.`
+    },
+    {
+        id: 5,
+        nombre: "Lepanthes subulata",
+        imagen: "img/objSubulata.jpg",
+        habito: "Hierba, Epífita",
+        size: '0,12” [3 mm]',
+        familia: "Orchidaceae",
+        origen: "Endémica",
+        region: "Andes",
+        elevacion: "Aprox. 1850 msnm",
+        departamentos: "Antioquia",
+        conservacion: "No Evaluada",
+        descripcion: "Epífita de tamaño miniatura, de crecimiento fresco, con ramicauls suberectos y muy delgados, envueltos por entre 7 y 11 vainas lepantiformes microscópicamente escabrosas, finas, cercanas y que portan una sola vaina, apical, de erecta a suberecta. finamente coriácea, reticulada, elíptica-ovada, aguda, contratada apicalmente en un segmento lineal, tridenticulada, diente medio más largo, ampliamente cuneada por debajo en la base peciolada, florece en una hoja filiforme, sucesivamente muchas inflorescencias florales que surgen en la parte superior de la hoja.",
+        referencias: `Farfán et al. 2003, Viveros & Higgins 2007, Govaerts et al. 2012, Luer & Thoerle 2012
+        Viveros, P. , C.A. Luer & M. Celis 2023-10-10. Lepanthes subulata Luer & R.Escobar En Bernal, R., S.R. Gradstein & M. Celis (eds.). 2015. Catálogo de plantas y líquenes de Colombia. Instituto de Ciencias Naturales, Universidad Nacional de Colombia, Bogotá. http://catalogoplantasdecolombia.unal.edu.co
+        W3 Tropicos, Kew Monocot list , IPNI ; Phytologia 54:327 Luer 1983; AOS Bulletin 53[9]: 922, Luer & Escobar 1984.`
+    }
+]
+
+const listaPokemon = document.querySelector("#listaPokemon");
+const modal = document.getElementById("mainModal");
+
+for (let i = 0; i < orquideas.length; i++) {
+    mostrarPokemon(orquideas[i]);
+}
+
+function mostrarPokemon(orquidea) {
+    const div = document.createElement("div");
+    div.classList.add("pokemon");
+    div.innerHTML = `
+        <p class="pokemon-id-back">#${orquidea.id}</p>
+        <div class="pokemon-imagen">
+            <img class="imgOrquidea" src="${orquidea.imagen}" alt="${orquidea.nombre}">
+            <button class="btn btn-ver" id="${orquidea.id}">ver</button>
+        </div>
+        <div class="pokemon-info">
+            <div class="nombre-contenedor">
+                <p class="pokemon-id">#${orquidea.id}</p>
+                <em><h2 class="pokemon-nombre">${orquidea.nombre}</h2></em>
+            </div>
+            <div class="pokemon-tipos">
+                <p class="electric tipo">Electrico</p>
+                <p class="fighting tipo">Lucha</p>
+            </div>
+            <div class="pokemon-stats">
+                <p class="stat">Región Biogeográfica</p>
+                <p class="stat-text">${orquidea.region}</p>
+                <p class="stat">Elevación</p>
+                <p class="stat-text">${orquidea.elevacion}</p>
+            </div>
+        </div>
+    `;
+    listaPokemon.append(div);
+}
+
+const botonesVer = document.querySelectorAll(".btn-ver");
+
+botonesVer.forEach(boton => boton.addEventListener("click", (event) => {
+    const botonId = event.currentTarget.id;
+    const resultado = orquideas.find(orquidea => orquidea.id == botonId);
+    /* console.log(resultado); */
+    modal.showModal();
+    modal.innerHTML = `
+    <div class="pokemon-imagen">
+        <img class="imgModal" src="${resultado.imagen}" alt="${resultado.nombre}">
+    </div>
+    <div class="pokemon-info">
+        <div class="nombre-contenedor">
+            <em><h2 class="pokemon-nombre">${resultado.nombre}</h2></em>
+        </div>
+        <div class="pokemon-tipos">
+            <p class="electric tipo">Electrico</p>
+            <p class="fighting tipo">Lucha</p>
+        </div>
+        <div class="pokemon-stats">
+            <p class="stat">Hábito</p>
+            <p class="stat-text">${resultado.habito}</p>
+            <p class="stat">Tamaño de la flor</p>
+            <p class="stat-text">${resultado.size}</p>
+            <p class="stat">Familia</p>
+            <p class="stat-text">${resultado.familia}</p>
+            <p class="stat">Origen</p>
+            <p class="stat-text">${resultado.origen}</p>
+            <p class="stat">Región Biogeográfica</p>
+            <p class="stat-text">${resultado.region}</p>
+            <p class="stat">Elevación</p>
+            <p class="stat-text">${resultado.elevacion}</p>
+            <p class="stat">Departamentos</p>
+            <p class="stat-text">${resultado.departamentos}</p>
+            <p class="stat">Estado de conersación</p>
+            <p class="stat-text">${resultado.conservacion}</p>
+            <p class="stat">Descripción</p>
+            <p class="stat-text">${resultado.descripcion}</p>
+            <p class="stat">Referencias</p>
+            <p class="stat-text">${resultado.referencias}</p>
+        </div>
+        <button class="btn btn-cerrar" onclick="closeModal()">Cerrar</button>
+    </div>
+`;
+}))
+
+const closeModal = () => {
+    modal.close();
+}
